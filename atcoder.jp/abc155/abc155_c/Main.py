49206c1bd8a6620d99c6f01ff5d5e0343919fea9
DY811 <teddygirl504@gmail.com>
2021-04-09 13:05:12 +0900

    [AC] abc155 abc155_c

---
+++ atcoder.jp/abc155/abc155_c/Main.py
@@ -0,0 +1,22 @@
+import sys
+import collections
+"""
+read = sys.stdin.buffer.read #標準入力から複数行取得するための関数です。 readlines() が結果を行単位で分割してリストで返すのに対して、 read() は結果をそのまま1つの文字列として取得します。
+readline = sys.stdin.buffer.readline #標準入力から1行取得するための関数です。
+readlines = sys.stdin.buffer.readlines #標準入力から複数行取得するための関数です。戻り値はリストで、入力された文字列が1行ずつ要素として格納されています。
+"""
+
+def solve():
+    readline = sys.stdin.buffer.readline
+    mod = 10 ** 9 + 7
+    n = int(readline())
+    s = collections.Counter([str(readline().rstrip().decode('utf-8')) for _ in range(n)]).most_common()
+    s.sort(key=lambda x: x[0])
+    s.sort(key=lambda x: x[1], reverse=True)
+    for k, v in s:
+        if v == s[0][1]:
+            print(k)
+        else:
+            break
+if __name__ == '__main__':
+    solve()
